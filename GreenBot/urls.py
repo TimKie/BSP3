9c832b952b7b417e29e18864b8e5b5682754ce37
TimKie <55922216+TimKie@users.noreply.github.com>
2020-09-23 18:44:42 +0200

    statistics added

--- GreenBot/urls.py
+++ GreenBot/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.home, name='GreenBot-home'),
     path('about/', views.about, name='GreenBot-about'),
+    path('statistics/', views.ChartView.as_view(), name='GreenBot-statistics')
 ]
--- GreenBot/views.py
+++ GreenBot/views.py
@@ -1,32 +1,40 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
+from django.views.generic import TemplateView
 
 products = [
     {
         'category': 'Meat',
         'name': 'Steak',
+        'price': 10,
     },
     {
         'category': 'Vegetables',
         'name': 'Tomato',
+        'price': 5,
     },
     {
         'category': 'Water',
         'name': 'Viva',
+        'price': 2,
     },
     {
         'category': 'Milk',
         'name': 'Luxlait',
+        'price': 3,
     },
     {
         'category': 'Meat',
         'name': 'Chicken Nuggets',
+        'price': 7,
     },
     {
         'category': 'Vegetables',
         'name': 'Salad',
+        'price': 2,
     },
 ]
+
 
 @login_required()
 def home(request):
@@ -35,6 +43,16 @@ def home(request):
     }
     return render(request, 'GreenBot/home.html', context)
 
+
 @login_required()
 def about(request):
     return render(request, 'GreenBot/about.html', {'title': 'About'})
+
+
+class ChartView(TemplateView):
+    template_name = 'GreenBot/statistics.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["qs"] = products
+        return context
