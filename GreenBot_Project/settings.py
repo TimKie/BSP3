2a63294ff2520a7baf30ed08ba83132caf0efa8e
TimKie <55922216+TimKie@users.noreply.github.com>
2020-10-09 16:39:26 +0200

    PostgreSQL database added

--- GreenBot_Project/settings.py
+++ GreenBot_Project/settings.py
@@ -78,8 +78,12 @@ WSGI_APPLICATION = 'GreenBot_Project.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        "NAME": "testdb",
+        "USER": "tim",
+        "PASSWORD": "",
+        "HOST": "localhost",
+        "PORT": "5432",
     }
 }
 
