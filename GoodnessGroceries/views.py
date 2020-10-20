76fd589f0dc5df9adad316baf39e798f645a3a69
TimKie <55922216+TimKie@users.noreply.github.com>
2020-10-20 16:27:44 +0200

    added static csv files

--- GoodnessGroceries/views.py
+++ GoodnessGroceries/views.py
@@ -2,18 +2,18 @@ from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 from django.views.generic import TemplateView
 
-# ----------- import csv file of products and convert it into a list of dictionaries -----------
+# ----------- import csv a file and convert it into a list of dictionaries --------------------
 import csv
 
 
-def load_csv_file():
-    list_of_products = []
+def load_csv_file(file_name):
+    output_list = []
 
-    data = csv.DictReader(open("list_of_products.csv", encoding='utf-8-sig'), delimiter=';')
+    data = csv.DictReader(open(file_name, encoding='utf-8-sig'), delimiter=',')
 
     for d in data:
-        list_of_products.append(d)
-    return list_of_products
+        output_list.append(d)
+    return output_list
 # ----------------------------------------------------------------------------------------------
 
 
@@ -30,7 +30,7 @@ def about(request):
 @login_required()
 def product_overview(request):
     context = {
-        'products': load_csv_file()
+        'products': load_csv_file("static_csv_files/products.csv")
     }
     return render(request, 'GoodnessGroceries/product_overview.html', context)
 
@@ -40,7 +40,7 @@ class MostExpensiveProducts(TemplateView):
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context["qs"] = load_csv_file()
+        context["qs"] = load_csv_file("static_csv_files/products.csv")
         return context
 
 
@@ -49,7 +49,7 @@ class MostSoldProducts(TemplateView):
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context["qs"] = load_csv_file()
+        context["qs"] = load_csv_file("static_csv_files/products.csv")
         return context
 
 
@@ -57,8 +57,9 @@ number_of_indicators = {}
 class MostPopularIndicators(TemplateView):
     # ---- Get number of occurrences of the indicators -------------------------
     list_of_indicators = []
-    for product in load_csv_file():
-        for indicator in product['product_indicators'].split(","):
+    for product in load_csv_file("static_csv_files/products.csv"):
+        for i in range(3):
+            indicator = product['indicators/'+str(i)+'/indicator_id']
             list_of_indicators.append(indicator)
             number_of_indicators[indicator] = list_of_indicators.count(indicator)
     # --------------------------------------------------------------------------
@@ -74,9 +75,9 @@ number_of_product_types = {}
 class MostPopularProductTypes(TemplateView):
     # ---- Get number of occurrences of the product types -------------------------
     list_of_product_types = []
-    for product in load_csv_file():
-        list_of_product_types.append(product['product_type'])
-        number_of_product_types[product['product_type']] = list_of_product_types.count(product['product_type'])
+    for product in load_csv_file("static_csv_files/products.csv"):
+        list_of_product_types.append(product['type'])
+        number_of_product_types[product['type']] = list_of_product_types.count(product['type'])
     # --------------------------------------------------------------------------
     template_name = 'GoodnessGroceries/most_popular_product_types.html'
 
@@ -84,3 +85,4 @@ class MostPopularProductTypes(TemplateView):
         context = super().get_context_data(**kwargs)
         context["qs"] = number_of_product_types
         return context
+
