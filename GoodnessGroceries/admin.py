105d01b52a9fa9458073f29b510142058fae0322
Flavio <flavio8699@gmail.com>
2021-07-12 11:11:04 +0200

    Add APNs and modify API endpoints

--- GoodnessGroceries/admin.py
+++ GoodnessGroceries/admin.py
@@ -2,9 +2,9 @@ from django.contrib import admin
 from .models import *
 
 admin.site.register(CashierTicketProducts)
-admin.site.register(MonitoringData)
 admin.site.register(ProductReviews)
 admin.site.register(Users)
 admin.site.register(StaticProducts)
 admin.site.register(StaticIndicators)
 admin.site.register(StaticIndicatorCategories)
+admin.site.register(Devices)
