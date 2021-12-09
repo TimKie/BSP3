067493dd4fb946d406417f3e22c1c4f5e15f7187
Binzinz <58930750+Binzinz@users.noreply.github.com>
2021-12-09 09:49:15 +0100

    Update settings.py

--- GoodnessGroceries_Project/settings.py
+++ GoodnessGroceries_Project/settings.py
@@ -135,8 +135,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_ROOT = (Path(BASE_DIR) / "static").resolve()
 
-# STATICFILES_DIRS = [
-#    os.path.join(BASE_DIR, "static"),
+ STATICFILES_DIRS =  (os.path.join(BASE_DIR, "static"),)
 #    os.path.join(BASE_DIR, "static_csv_files")
 # ]
 
