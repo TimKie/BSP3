e47057e5d0e9ef3901c7534c4b2602d7bd091e86
Binzinz <58930750+Binzinz@users.noreply.github.com>
2022-02-25 13:23:32 +0100

    Update fetch_phase2.py

--- GoodnessGroceries/management/commands/fetch_phase2.py
+++ GoodnessGroceries/management/commands/fetch_phase2.py
@@ -9,27 +9,23 @@ class Command(BaseCommand):
     def handle(self, *args, **kwargs):
         for user in Users.objects.filter(status='valid'):
             if user.phase2_date:
-                print(user.phase2_date)
                 if user.phase2_date.strftime('%Y-%m-%d') == (datetime.now()).strftime('%Y-%m-%d'):
-                     print('User' + user.participant_id + ' has iOS')
-                
-            if user.phase2_date == (datetime.now()).strftime('%Y-%m-%d'):
-                if user.platform == 'ios':
-                    print('User' + user.participant_id + ' has iOS')
-                    for device in APNSDevice.objects.filter(name=user.participant_id):
-                        print('Notif sent to' + user.participant_id)
-                        device.send_message("", extra={
-                            'aps': {
-                                'mutable-content': 1,
-                                'alert': {
-                                    'title': 'NOTIFICATION_ACCOUNT_PHASE2_TITLE',
-                                    'body': 'NOTIFICATION_ACCOUNT_PHASE2_BODY'
-                                },
-                                'sound': 'default',
-                                'badge': 1
-                            }
-                        })
-                elif user.platform == 'android':
-                    # TODO
-                    pass
+                    if user.platform == 'ios':
+                        print('User' + user.participant_id + ' has iOS')
+                        for device in APNSDevice.objects.filter(name=user.participant_id):
+                            print('Notif sent to' + user.participant_id)
+                            device.send_message("", extra={
+                                'aps': {
+                                    'mutable-content': 1,
+                                    'alert': {
+                                        'title': 'NOTIFICATION_ACCOUNT_PHASE2_TITLE',
+                                        'body': 'NOTIFICATION_ACCOUNT_PHASE2_BODY'
+                                    },
+                                    'sound': 'default',
+                                    'badge': 1
+                                }
+                            })
+                    elif user.platform == 'android':
+                        # TODO
+                        pass
                 
