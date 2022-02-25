b7274f2e4a127b13a88b01aeb1cc8ca9d7eafe66
Binzinz <58930750+Binzinz@users.noreply.github.com>
2022-02-25 10:29:40 +0100

    Update fetch_phase2.py

--- GoodnessGroceries/management/commands/fetch_phase2.py
+++ GoodnessGroceries/management/commands/fetch_phase2.py
@@ -1,1 +1,96 @@
+from django.core.management.base import BaseCommand
+from GoodnessGroceries.models import Users
+import os
+import csv
+from push_notifications.models import APNSDevice, GCMDevice
+import requests
 
+class Command(BaseCommand):
+    help = 'Send notif when Phase 2 start.'
+
+    def handle(self, *args, **kwargs):
+        for participant in Users.objects.filter(reviewed=False).distinct('participant'):
+            if participant.platform == 'ios':
+                for device in APNSDevice.objects.filter(name=participant.participant_id):
+                    device.send_message("", extra={
+                        'aps': {
+                            'mutable-content': 1,
+                            'alert': {
+                                'title': 'NOTIFICATION_ACCOUNT_PHASE2_TITLE',
+                                'body': 'NOTIFICATION_ACCOUNT_PHASE2_BODY'
+                            },
+                            'sound': 'default',
+                            'badge': len(products)
+                        },
+                        'products': list(map(lambda x: str(x), products))
+                    })
+                print(list(map(lambda x: str(x), products)))
+            elif participant.platform == 'android':
+                # TODO
+                pass
+        
+        
+        
+        
+        
+        
+        # TODO: configuration of the CSV file
+        date_column = 0
+        time_column = 1
+        participant_column = 2
+        product_ref_column = 3
+        product_column = 4
+        quantity_column = 5
+        price_column = 6
+        store_column = 7
+                        
+        file_url = 'https://dropit.uni.lu/invitations?share=ce550b5b315efd73b171&dl=1'
+
+        with requests.Session() as s:
+            download = s.get(file_url)
+
+            decoded_content = download.content.decode('utf-8')
+
+            cr = csv.reader(decoded_content.splitlines(), delimiter=';')
+            header = next(cr)
+            if header != None:
+                for row in cr:
+                    print(row)
+                    try:
+                        participant = Users.objects.only(
+                            'participant_id').get(participant_id='220000'+row[participant_column])
+                    except Users.DoesNotExist:
+                        continue
+                    timestamp = row[date_column][0:4]+'-'+row[date_column][4:6]+'-'+row[date_column][6:8]+' '+row[time_column][0:2]+':'+row[time_column][3:5]+':'+row[time_column][6:8]
+                    product_ean = row[product_ref_column]
+                    ticket = CashierTicketProducts.objects.create(
+                        participant=participant, timestamp=timestamp, product_ean=product_ean)
+
+        productsToReview = {}
+        for cashier_ticket in CashierTicketProducts.objects.filter(reviewed=False).distinct('participant', 'product_ean'):
+            if not cashier_ticket.participant.participant_id in productsToReview:
+                productsToReview[cashier_ticket.participant.participant_id] = []
+            productsToReview[cashier_ticket.participant.participant_id].append(
+                cashier_ticket.product_ean)
+
+        for participant, products in productsToReview.items():
+            participant = Users.objects.get(participant_id=participant)
+
+            if participant.platform == 'ios':
+                for device in APNSDevice.objects.filter(name=participant.participant_id):
+                    device.send_message("", extra={
+                        'aps': {
+                            'mutable-content': 1,
+                            'alert': {
+                                'title': 'NOTIFICATION_REVIEW_PRODUCTS_TITLE',
+                                'body': 'NOTIFICATION_REVIEW_PRODUCTS_BODY'
+                            },
+                            'sound': 'default',
+                            'badge': len(products)
+                        },
+                        'products': list(map(lambda x: str(x), products))
+                    })
+                print(list(map(lambda x: str(x), products)))
+            elif participant.platform == 'android':
+                # TODO
+                pass
