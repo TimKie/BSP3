236603c526b05c3af9b1ac3d978fa0f683a68dea
Binzinz <58930750+Binzinz@users.noreply.github.com>
2022-02-25 11:46:27 +0100

    Update fetch_phase2.py

--- GoodnessGroceries/management/commands/fetch_phase2.py
+++ GoodnessGroceries/management/commands/fetch_phase2.py
@@ -8,6 +8,7 @@ class Command(BaseCommand):
 
     def handle(self, *args, **kwargs):
         for user in Users.objects.filter(status='valid'):
+            print('User' + user.participant_id + ' has ' + user.platform)
             if user.phase2_date == (datetime.now()).strftime('%Y-%m-%d'):
                 if user.platform == 'ios':
                     print('User' + user.participant_id + ' has iOS')
