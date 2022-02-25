d48093b4b76d7375d86e9c40da4be72950cb793e
Binzinz <58930750+Binzinz@users.noreply.github.com>
2022-02-25 08:41:48 +0100

    Update views.py

--- GoodnessGroceries/views.py
+++ GoodnessGroceries/views.py
@@ -15,7 +15,7 @@ from .models import *
 from .functions import handle_product_reviews
 from datetime import datetime
 from datetime import timedelta
-from push_notifications.models import APNSDevice, PCMDevice
+from push_notifications.models import APNSDevice
 
 # ----------- import csv a file and convert it into a list of dictionaries --------------------
 import csv
