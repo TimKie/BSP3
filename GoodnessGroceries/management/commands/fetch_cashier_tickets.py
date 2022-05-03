c6484fc1e28ff6d641f4487ae0d883201e5bd515
Binzinz <58930750+Binzinz@users.noreply.github.com>
2022-05-03 15:58:02 +0200

    Update fetch_cashier_tickets.py

--- GoodnessGroceries/management/commands/fetch_cashier_tickets.py
+++ GoodnessGroceries/management/commands/fetch_cashier_tickets.py
@@ -20,56 +20,58 @@ class Command(BaseCommand):
         price_column = 6
         store_column = 7
                         
-        file_url = '/home/pall_user/tickets.csv'
-        if path.exists(file_url):
-            #with requests.Session() as s:
-            #download = s.get(file_url)
-            file = open(file_url, 'r')
-            content = file.read()
-            file.close()
-            os.rename('/home/pall_user/tickets.csv', '/home/pall_user/tickets_fetched.csv')
-            decoded_content = content.content.decode('utf-8')
+        directory = '/home/admin/tickets'
+        directory_done = '/home/admin/tickets'
+        for filename in os.listdir(directory):
+            f = os.path.join(directory, filename)
+            # checking if it is a file
+            if os.path.isfile(f):
+                file = open(f, 'r')
+                content = file.read()
+                file.close()
+                os.rename(os.path.join(directory, filename), os.path.join(directory_done, filename))
+                decoded_content = content.content.decode('utf-8')
 
-            cr = csv.reader(decoded_content.splitlines(), delimiter=';')
-            header = next(cr)
-            if header != None:
-                for row in cr:
-                    print(row)
-                    try:
-                        participant = Users.objects.only(
-                            'participant_id').get(participant_id='220000'+row[participant_column])
-                    except Users.DoesNotExist:
-                        continue
-                    timestamp = row[date_column][0:4]+'-'+row[date_column][4:6]+'-'+row[date_column][6:8]+' '+row[time_column][0:2]+':'+row[time_column][3:5]+':'+row[time_column][6:8]
-                    product_ean = row[product_ref_column]
-                    ticket = CashierTicketProducts.objects.create(
-                        participant=participant, timestamp=timestamp, product_ean=product_ean)
+                cr = csv.reader(decoded_content.splitlines(), delimiter=';')
+                header = next(cr)
+                if header != None:
+                    for row in cr:
+                        print(row)
+                        try:
+                            participant = Users.objects.only(
+                                'participant_id').get(participant_id='220000'+row[participant_column])
+                        except Users.DoesNotExist:
+                            continue
+                        timestamp = row[date_column][0:4]+'-'+row[date_column][4:6]+'-'+row[date_column][6:8]+' '+row[time_column][0:2]+':'+row[time_column][3:5]+':'+row[time_column][6:8]
+                        product_ean = row[product_ref_column]
+                        ticket = CashierTicketProducts.objects.create(
+                            participant=participant, timestamp=timestamp, product_ean=product_ean)
 
-            productsToReview = {}
-            for cashier_ticket in CashierTicketProducts.objects.filter(reviewed=False).distinct('participant', 'product_ean'):
-                if not cashier_ticket.participant.participant_id in productsToReview:
-                    productsToReview[cashier_ticket.participant.participant_id] = []
-                productsToReview[cashier_ticket.participant.participant_id].append(
-                    cashier_ticket.product_ean)
+                productsToReview = {}
+                for cashier_ticket in CashierTicketProducts.objects.filter(reviewed=False).distinct('participant', 'product_ean'):
+                    if not cashier_ticket.participant.participant_id in productsToReview:
+                        productsToReview[cashier_ticket.participant.participant_id] = []
+                    productsToReview[cashier_ticket.participant.participant_id].append(
+                        cashier_ticket.product_ean)
 
-            for participant, products in productsToReview.items():
-                participant = Users.objects.get(participant_id=participant)
+                for participant, products in productsToReview.items():
+                    participant = Users.objects.get(participant_id=participant)
 
-                if participant.platform == 'ios':
-                    for device in APNSDevice.objects.filter(name=participant.participant_id):
-                        device.send_message("", extra={
-                            'aps': {
-                                'mutable-content': 1,
-                                'alert': {
-                                    'title': 'NOTIFICATION_REVIEW_PRODUCTS_TITLE',
-                                    'body': 'NOTIFICATION_REVIEW_PRODUCTS_BODY'
+                    if participant.platform == 'ios':
+                        for device in APNSDevice.objects.filter(name=participant.participant_id):
+                            device.send_message("", extra={
+                                'aps': {
+                                    'mutable-content': 1,
+                                    'alert': {
+                                        'title': 'NOTIFICATION_REVIEW_PRODUCTS_TITLE',
+                                        'body': 'NOTIFICATION_REVIEW_PRODUCTS_BODY'
+                                    },
+                                    'sound': 'default',
+                                    'badge': len(products)
                                 },
-                                'sound': 'default',
-                                'badge': len(products)
-                            },
-                            'products': list(map(lambda x: str(x), products))
-                        })
-                    print(list(map(lambda x: str(x), products)))
-                elif participant.platform == 'android':
-                    # TODO
-                    pass
+                                'products': list(map(lambda x: str(x), products))
+                            })
+                        print(list(map(lambda x: str(x), products)))
+                    elif participant.platform == 'android':
+                        # TODO
+                        pass
