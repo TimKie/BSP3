bd44e5ef52c719e8aa7d95ac667b82760cb8ecf7
Binzinz <58930750+Binzinz@users.noreply.github.com>
2022-05-06 11:23:08 +0200

    Update views.py

--- GoodnessGroceries/views.py
+++ GoodnessGroceries/views.py
@@ -174,8 +174,15 @@ def update_status_of_user(request, participant_id):
         elif user.platform == 'android':
              for device in GCMDevice.objects.filter(name=user.participant_id):
                 device.send_message("", extra={
-                    'title': 'NOTIFICATION_ACCOUNT_AUTHENTICATED_TITLE',
-                    'body': 'NOTIFICATION_ACCOUNT_AUTHENTICATED_BODY'
+                    'aps': {
+                        'mutable-content': 1,
+                        'alert': {
+                            'title': 'NOTIFICATION_ACCOUNT_AUTHENTICATED_TITLE',
+                            'body': 'NOTIFICATION_ACCOUNT_AUTHENTICATED_BODY'
+                        },
+                        'sound': 'default',
+                        'badge': 1
+                    }
                 })
     user.save()
 
@@ -216,9 +223,16 @@ def update_status_of_user_archived(request, participant_id):
         elif user.platform == 'android':
             for device in GCMDevice.objects.filter(name=user.participant_id):
                 device.send_message("", extra={
-                    'title': 'NOTIFICATION_ACCOUNT_ARCHIVED_TITLE',
-                    'body': 'NOTIFICATION_ACCOUNT_ARCHIVED_BODY'
-               })
+                      'aps': {
+                        'mutable-content': 1,
+                        'alert': {
+                            'title': 'NOTIFICATION_ACCOUNT_ARCHIVED_TITLE',
+                            'body': 'NOTIFICATION_ACCOUNT_ARCHIVED_BODY'
+                        },
+                        'sound': 'default',
+                        'badge': 1
+                    }            
+                })
     user.save()
 
     users = Users.objects.all()
@@ -252,8 +266,15 @@ def update_status_of_user_phase2(request, participant_id):
         elif user.platform == 'android':
             for device in GCMDevice.objects.filter(name=user.participant_id):
                 device.send_message("", extra={
-                    'title': 'NOTIFICATION_ACCOUNT_PHASE2_TITLE',
-                    'body': 'NOTIFICATION_ACCOUNT_PHASE2_BODY'
+                    'aps': {
+                        'mutable-content': 1,
+                        'alert': {
+                            'title': 'NOTIFICATION_ACCOUNT_PHASE2_TITLE',
+                            'body': 'NOTIFICATION_ACCOUNT_PHASE2_BODY'
+                        },
+                        'sound': 'default',
+                        'badge': 1
+                    }
                 })
     user.save()
 
