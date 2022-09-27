661f335460a9a686c15ae3a6600900704ede34cb
Binzinz <58930750+Binzinz@users.noreply.github.com>
2022-09-27 15:34:04 +0200

    Update fetch_cashier_tickets.py

--- GoodnessGroceries/management/commands/fetch_cashier_tickets.py
+++ GoodnessGroceries/management/commands/fetch_cashier_tickets.py
@@ -45,13 +45,14 @@ class Command(BaseCommand):
                         timestamp = row[date_column][0:4]+'-'+row[date_column][4:6]+'-'+row[date_column][6:8]+' '+row[time_column][0:2]+':'+row[time_column][3:5]+':'+row[time_column][6:8]
                         if participant.status == 'valid' and participant.phase2_date.strftime('%Y-%m-%d') <= (datetime.now()).strftime('%Y-%m-%d'):
                             print(CashierTicketProducts.objects.filter(participant=participant,product_ean=product_ean).count())
-                            if CashierTicketProducts.objects.filter(participant=participant,product_ean=product_ean).count() <=2:
+                            if CashierTicketProducts.objects.filter(participant=participant,product_ean=product_ean).count() <2:
                                 ticket = CashierTicketProducts.objects.create(participant=participant, timestamp=timestamp, product_ean=product_ean)
                 productsToReview = {}
-                for cashier_ticket in CashierTicketProducts.objects.filter(reviewed=False).distinct('participant', 'product_ean'):
+                for cashier_ticket in CashierTicketProducts.objects.filter(reviewed=False, notified=False).distinct('participant', 'product_ean'):
                     if not cashier_ticket.participant.participant_id in productsToReview:
                         productsToReview[cashier_ticket.participant.participant_id] = []
                     productsToReview[cashier_ticket.participant.participant_id].append(cashier_ticket.product_ean)
+                    cashier_ticket.notified=True
 
                 for participant, products in productsToReview.items():
                     participant = Users.objects.get(participant_id=participant)
