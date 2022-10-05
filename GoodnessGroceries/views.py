327584364de2785acc5faa987f1b2d9be931bdf6
Binzinz <58930750+Binzinz@users.noreply.github.com>
2022-10-05 09:38:31 +0200

    Update views.py

--- GoodnessGroceries/views.py
+++ GoodnessGroceries/views.py
@@ -318,17 +318,20 @@ def update_status_of_user_phase2(request, participant_id):
         user.phase2_date = (datetime.now()).strftime('%Y-%m-%d')
         if user.platform == 'ios':
             for device in APNSDevice.objects.filter(name=user.participant_id):
-                device.send_message("", extra={
-                    'aps': {
-                        'mutable-content': 1,
-                        'alert': {
-                            'title': 'NOTIFICATION_ACCOUNT_PHASE2_TITLE',
-                            'body': 'NOTIFICATION_ACCOUNT_PHASE2_BODY'
-                        },
-                        'sound': 'default',
-                        'badge': 1
-                    }
-                })
+                try:
+                    device.send_message("", extra={
+                        'aps': {
+                            'mutable-content': 1,
+                            'alert': {
+                                'title': 'NOTIFICATION_ACCOUNT_PHASE2_TITLE',
+                                'body': 'NOTIFICATION_ACCOUNT_PHASE2_BODY'
+                            },
+                            'sound': 'default',
+                            'badge': 1
+                        }
+                    })
+                except:
+                    pass
         elif user.platform == 'android':
             for device in GCMDevice.objects.filter(name=user.participant_id):
                 try:
