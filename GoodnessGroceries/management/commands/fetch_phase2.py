d9dcdc77b6d0d392455c4191c686a7feee7b98f1
Binzinz <58930750+Binzinz@users.noreply.github.com>
2022-10-05 09:34:50 +0200

    Update fetch_phase2.py

--- GoodnessGroceries/management/commands/fetch_phase2.py
+++ GoodnessGroceries/management/commands/fetch_phase2.py
@@ -12,17 +12,21 @@ class Command(BaseCommand):
                 if user.phase2_date.strftime('%Y-%m-%d') == (datetime.now()).strftime('%Y-%m-%d'):
                     if user.platform == 'ios':
                         for device in APNSDevice.objects.filter(name=user.participant_id):
-                            device.send_message("", extra={
-                                'aps': {
-                                    'mutable-content': 1,
-                                    'alert': {
-                                        'title': 'NOTIFICATION_ACCOUNT_PHASE2_TITLE',
-                                        'body': 'NOTIFICATION_ACCOUNT_PHASE2_BODY'
-                                    },
-                                    'sound': 'default',
-                                    'badge': 1
-                                }
-                            })
+                             try:   
+                                device.send_message("", extra={
+                                    'aps': {
+                                        'mutable-content': 1,
+                                        'alert': {
+                                            'title': 'NOTIFICATION_ACCOUNT_PHASE2_TITLE',
+                                            'body': 'NOTIFICATION_ACCOUNT_PHASE2_BODY'
+                                        },
+                                        'sound': 'default',
+                                        'badge': 1
+                                    }
+                                })
+                             except:
+                                pass
+                                
                     elif user.platform == 'android':
                        for device in GCMDevice.objects.filter(name=user.participant_id):
                             try:    
