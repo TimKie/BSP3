6543816ef1984800edfbd61cfca45a42a026ec6f
Binzinz <58930750+Binzinz@users.noreply.github.com>
2023-01-13 15:01:26 +0100

    Update fetch_cashier_tickets.py

--- GoodnessGroceries/management/commands/fetch_cashier_tickets.py
+++ GoodnessGroceries/management/commands/fetch_cashier_tickets.py
@@ -52,8 +52,8 @@ class Command(BaseCommand):
                 for cashier_ticket in CashierTicketProducts.objects.filter(reviewed=False, notified=False).distinct('participant', 'product_ean'):
                     if not cashier_ticket.participant.participant_id in productsToReview:
                         productsToReview[cashier_ticket.participant.participant_id] = []
+                    print(cashier_ticket.timestamp.strftime('%Y-%m-%d'))
                     if participant.phase2_date.strftime('%Y-%m-%d') <= cashier_ticket.timestamp.strftime('%Y-%m-%d'):
-                        print(cashier_ticket.id+"notified")
                         productsToReview[cashier_ticket.participant.participant_id].append(cashier_ticket.product_ean)
                         cashier_ticket.notified=True
                         cashier_ticket.save()
