64c59fe1b8ddfab83131bd46b9049066813d1aa9
Binzinz <58930750+Binzinz@users.noreply.github.com>
2023-03-10 14:30:01 +0100

    Create awards_notif.py

---
+++ GoodnessGroceries/management/commands/awards_notif.py
@@ -0,0 +1,44 @@
+from django.core.management.base import BaseCommand
+from GoodnessGroceries.models import Users
+from push_notifications.models import APNSDevice
+from datetime import datetime
+
+class Command(BaseCommand):
+    help = 'Send notif for Awards.'
+
+    def handle(self, *args, **kwargs):
+        for user in Users.objects.filter(status='valid'):
+            if user.id == "2200000000006":
+                    if user.platform == 'ios':
+                        for device in APNSDevice.objects.filter(name=user.participant_id):
+                             try:   
+                                device.send_message("", extra={
+                                    'aps': {
+                                        'mutable-content': 1,
+                                        'alert': {
+                                            'title': 'Gagnez 150€',
+                                            'body': 'Donnez votre feedback'
+                                        },
+                                        'sound': 'default',
+                                        'badge': 1
+                                    }
+                                })
+                             except:
+                                pass
+                                
+                    elif user.platform == 'android':
+                       for device in GCMDevice.objects.filter(name=user.participant_id):
+                            try:    
+                                device.send_message(None, extra={
+                                    'data': {
+                                        'mutable-content': 1,
+                                        'alert': {
+                                            'title': 'Gagnez 150€',
+                                            'body': 'Donnez votre feedback'
+                                        },
+                                        'sound': 'default',
+                                        'badge': 1
+                                    }
+                                })
+                            except:
+                                pass
